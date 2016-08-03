f25d31d6478820381829bf3c8900920e2ca838b1
Fu Wei <softbaddog@gmail.com>
2016-08-03 18:06:30 +0800

    修改调光指令命令

--- app/controllers/nodes.server.controller.js
+++ app/controllers/nodes.server.controller.js
@@ -1,4 +1,6 @@
 var Node = require('mongoose').model('Node');
+var http = require('http');
+var querystring = require('querystring');
 
 var getErrorMessage = function(err) {
 	if (err.errors) {
@@ -59,8 +61,55 @@ exports.read = function(req, res) {
 exports.update = function(req, res) {
 	var node = req.node;
 
-	node.name = req.body.name;
-	node.online = req.body.online;
+	if (req.body.name && req.body.name != node.name) {
+		node.name = req.body.name;		
+	}
+
+	if (req.body.deviceId && req.body.deviceId != node.deviceId) {
+		node.deviceId = req.body.deviceId;
+	}
+	
+	if (req.body.groupId && req.body.groupId != node.groupId) {
+		node.groupId = req.body.groupId;		
+	}
+
+	if (req.body.status && req.body.status != node.status) {
+		node.status = req.body.status;
+	}
+
+	if (req.body.parent & req.body.parent != node.parent) {
+		node.parent = req.body.parent;		
+	}
+
+	if (req.body.level && node.level != req.body.level) {
+			node.level = req.body.level;
+
+			// 如果刷新了调光级别，需要下发http request到EEM
+			var contents = querystring.stringify({
+				index: '0',
+				level: node.level,
+			});
+
+			console.log(contents);			
+			// var options = {
+			// 	host: 'localhost',
+			// 	port: '3000',
+			// 	path: '/iotdm/nb/v1/device/set/' + node.deviceId + 'urn:huawei:iotdm:device/data/huawei-iotdm-device-sensor:dim-level',
+			// 	method: 'POST',
+			// 	headers: {
+			// 		'Content-Type': 'application/x-www-form-urlencoded',
+			// 		'Content-Length': contents.length
+			// 	}
+			// };
+			// var newReq = http.request(options, function(res) {
+			// 	res.setEncoding('utf8');
+			// 	res.on('data', function(data) {
+			// 		console.log(data);
+			// 	});
+			// });
+			// newReq.write(contents);
+			// newReq.end();
+	}
 
 	node.save(function(err) {
 		if (err) {
