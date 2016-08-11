6844ea5aeb149b228570b5a9e141a1072a43a70c
Fu Wei <softbaddog@sina.com>
2016-08-11 18:11:05 +0800

    websocket事件对接代码刷新

--- config/socketio-client.js
+++ config/socketio-client.js
@@ -1,31 +1,46 @@
 var config = require('./config');
 var request = require('./request');
+var response = require('./response');
 
-module.exports = function() {
-	var socket = require('socket.io-client')('http://' + config.host + ':' + config.port);
+exports.initialize = function() {
+	var socket = require('socket.io-client')('http://' + config.host + ':' + config.socketPort);
+	// var socket = require('socket.io-client')('http://localhost:3000');
+	console.log('http://' + config.host + ':' + config.socketPort);
 	socket.on('connect', function() {
+		console.log('client connected');
 		socket.send('hello world');
 
-		// 与EEM连接成功后，每分钟获取一次所有设备状态
-		setInterval(request.post('dev-online-status', function(err, data) {
-			if (err) {
-				console.log(err);
-			}
-
-			// 解析数据并保存到数据库
-		}), 60000);
+		// 每10分钟获取一次所有设备状态
+		setInterval(function() {
+			request.get('dev-online-status', null, null, function(data) {
+				var devices = JSON.parse(data).online;
+				response.devStatus(devices);
+			});
+		}, 60000);
 
 		// 每10秒获取一次网络拓扑信息
-		setInterval(request.get('net-topo', function(err, data) {
-			if (err) {
-				console.log(err);
-			}
-
-			// 解析数据并保存到数据库
-		}), 10000);
-
-		socket.on('message', function(data) {
-			console.log(data);
-		});
+		setInterval(function() {
+			request.get('net-topo', function(data) {
+				var topo = JSON.parse(data);
+				console.log(topo);
+			});
+		}, 10000);
 	});
+
+	socket.on('message', function(data) {
+		console.log(data);
+	});
+
+	socket.on('error', function(err) {
+		console.log(err);
+	});
+
+	socket.on('disconnect', function() {
+		console.log('disconnect');
+	});
+
+	socket.on('reconnection', function(err) {
+		console.log('reconnection');
+	});
+
 };
