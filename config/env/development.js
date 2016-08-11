a153fc99dcdd1070c71fb45410f22d508703f5de
Fu Wei <softbaddog@sina.com>
2016-08-11 18:12:55 +0800

    填写eem地址

--- config/env/development.js
+++ config/env/development.js
@@ -1,8 +1,9 @@
 module.exports = {
 	db: "mongodb://localhost/hc-iot-6lowpan",
 	sessionSecret: "developmentSessionSecret",
-	host: "192.168.1.9",
-	port: "1884",
+	host: "10.93.145.59",
+	port: "8181",
+	socketPort: "8282",
 	gateway: "000D6F00055CCF08",
 	domain: "root"
 };
