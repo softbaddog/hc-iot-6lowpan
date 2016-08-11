912938f3c7ce8e28c6cf10a6ff75f835e849fc0f
Fu Wei <softbaddog@sina.com>
2016-08-11 18:08:17 +0800

    新增启动测试代码

--- server.js
+++ server.js
@@ -10,6 +10,26 @@ var pass = passport();
 
 app.listen(3000);
 
+require('./config/socketio-client').initialize();
+
+var request = require('./config/request');
+var response = require('./config/response');
+
+// setTimeout(function() {
+// 	request.get('dev-online-status', null, null, function(data) {
+// 		var devices = JSON.parse(data).online;
+// 		response.devStatus(devices);
+// 	});
+// }, 2000);
+
+// setTimeout(function() {
+// 	request.get('net-topo', null, null, function(data) {
+// 		var devices = JSON.parse(data.replace(/-/g, ''));
+// 		var map = response.devTopo(devices);
+// 		console.log(map);
+// 	});
+// }, 5000);
+
 module.exports = app;
 
 console.log("HC-IoT-6LoWPAN Server running at http://localhost:3000");
