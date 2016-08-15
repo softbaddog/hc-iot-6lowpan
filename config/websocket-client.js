859c24db5af368d2d55453aad7d0d1ba671060cc
Fu Wei <softbaddog@sina.com>
2016-08-15 21:03:09 +0800

    合入websocket处理代码

--- config/websocket-client.js
+++ config/websocket-client.js
@@ -1,11 +1,6 @@
 var config = require('./config');
 var request = require('./request');
 var response = require('./response');
-var connectSuccess = false;
-
-exports.success = function() {
-	return connectSuccess;
-};
 
 exports.initialize = function() {
 	var WebSocketClient = require('websocket').client;
@@ -20,46 +15,69 @@ exports.initialize = function() {
 
 	client.on('connect', function(connection) {
 		console.log('WebSocket Client Connected');
-		connectSuccess = true;
-
-		connection.on('error', function(error) {
-			console.log("Connection Error: " + error.toString());
-		});
-		connection.on('close', function() {
-			console.log('echo-protocol Connection Closed');
-		});
-		connection.on('message', function(message) {
-			if (message.type === 'utf8') {
-				console.log("Received: '" + message.utf8Data + "'");
-			}
-		});
 
 		// 获取在线状态
 		var devOnlineStatus = function() {
 			// data = '{ "online": [ "FFFF010203FCE000", "2E00000000000031", "2E00000000000030" ] }';
 			request.get('dev-online-status', null, null, function(data) {
 				var devices = JSON.parse(data);
-				console.log(devices);
 				response.devStatus(devices);
+				setTimeout(devOnlineStatus, 600000);
 			});
 		};
-		deveOnlineStatus();
-		setTimeout(devOnlineStatus, 60000);
+		devOnlineStatus();
 
 		// 获取拓扑数据
 		var netTopo = function() {
 			// data = '[ { "device-id": "FFFF010203FCE000", "status": "online", "node-id": 1, "parent-node-id": 0, "hop-count": 0 }, { "device-id": "2E00000000000030", "status": "online", "node-id": 2, "parent-node-id": 1, "hop-count": 1 }, { "device-id": "2E00000000000032", "status": "online", "node-id": 3, "parent-node-id": 2, "hop-count": 1 } ]';
 			request.get('net-topo', null, null, function(data) {
 				var devices = JSON.parse(data.replace(/-/g, ''));
-				console.log(devices);
 				var map = {};
 				response.devMap(devices, function(key, value) {
 					map[key] = value;
 				});
 				response.devTopo(devices, map);
+				setTimeout(netTopo, 150000);
 			});
 		};
 		netTopo();
-		setTimeout(netTopo, 5000);
+		
+		connection.on('error', function(error) {
+			console.log("Connection Error: " + error.toString());
+		});
+
+		connection.on('close', function() {
+			console.log('echo-protocol Connection Closed');
+		});
+		
+		// Received: '{"type":"huawei-iotdm-device-common:online-status-change","data":{"online-status":"offline"},"gateway":"000D6
+		// F00052AE47E","timestamp":"2016-08-15T15:27:55Z","esn":"2E00216EFC000255"}'
+		// Received: '{"type":"huawei-iotdm-device:data-report","data":{"huawei-iotdm-device-common:online-status":"offline","huawe
+		// i-iotdm-device-common:offline-reason":"normal"},"gateway":"000D6F00052AE47E","timestamp":"2016-08-15T15:27:55Z","esn":"2
+		// E00216EFC000255"}'
+
+
+		// Received: '{"type":"huawei-iotdm-device-common:online-status-change","data":{"on
+		// line-status":"online"},"gateway":"000D6F00052AE47E","timestamp":"2016-08-15T15:2
+		// 6:02Z","esn":"2E00216EFC000255"}'		
+		connection.on('message', function(message) {
+			if (message.type === 'utf8') {
+				var obj = JSON.parse(message.utf8Data.replace(/-/g, ''));
+				switch(obj.type) {
+					case 'huaweiiotdmdevicecommon:onlinestatuschange':
+						console.log("Received: '" + message.utf8Data + "'");
+						if (obj.data.onlinestatus === 'online') {
+							response.devStatusChanged(1, obj.esn);
+						} else {
+							response.devStatusChanged(0, obj.esn);
+						}
+						break;
+
+					default:
+						break;
+				}
+			}
+		});
+
 	});
 };
